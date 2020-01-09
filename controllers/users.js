4eecdd7329d9adba47780efca0aa0c7a381dd458
syfon01 <syfonisaac@gmail.com>
2020-01-09 02:46:40 +0100

    connection with database, creating API and using post man

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,27 @@
+var User = require("../models/User");
 module.exports.controller = (app) => {
-   // get users page
+  //  get all users
    app.get('/users', (req, res) => {
-     res.render('users', { title: 'Users',  description: 'This is the description of all the users' });
-   })
-  }
+     User.find({}, 'name email', function (error, users) {
+       if (error) {
+          console.log(error);
+      }
+       res.send(users);
+      })
+    }),
+
+     app.post('/users', (req, res) => {
+       const user = new User({
+         name: req.body.name,
+         email: req.body.email
+       })
+       user.save(function (error, user) {
+         
+         if (error) {
+           console.log(error);
+         }
+         res.send(user)
+       })
+     })
+
+    }
